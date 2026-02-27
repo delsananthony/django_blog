4d8357023e6dbb4d7f0950005e729d061ba7317a
delsananthony <delsan.anthony.das@gmail.com>
2026-02-27 16:50:41 +0800

    Improve Blog App: added register, login, logout and profile

--- README.md
+++ README.md
@@ -10,4 +10,7 @@
 - [Cryce Truly Django Course](https://www.youtube.com/watch?v=gAI218HSK8s&list=PLx-q4INfd95G-wrEjKDAcTB1K-8n1sIiz)
 - [Dennis Ivy Django Course](https://www.youtube.com/watch?v=xv_bwpA_aEA&list=PL-51WBLyFTg2vW-_6XBoUpE7vpmoR3ztO)
 - [Django Testing](https://www.youtube.com/watch?v=OfiCALrGE14&list=PL4cUxeGkcC9ic9O6xDW2d1qMp3rMOb0Nu)
+- [HTMX](https://htmx.org/docs/#introduction)
+- [Fast to load](https://www.youtube.com/watch?v=-Ln-8QM8KhQ)
+- [Being Fullstack](https://www.youtube.com/watch?v=LzMnsfqjzkA)
 - [](https://www.youtube.com/playlist?list=PLeLcvrwLe184Ws5cDLqQc1DxvuENJKlvf)
--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
